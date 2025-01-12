cd7befddfb1d169468623cfc019df847601c55b9
Pius Prince <piusprince61@gmail.com>
2025-01-12 21:00:38 +0000

    feat: add homepage and project schema types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import homepage from './documents/homepage'
+import project from './documents/project'
+
+export const schemaTypes = [
+  homepage,
+  project,
+  // Add more schema types here
+]
