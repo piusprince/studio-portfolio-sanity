c23cd86944026869a90c52ec09e5c3d96a63080f
Pius Prince <piusprince61@gmail.com>
2025-02-07 20:37:14 +0000

    feat: add presentation tool to Sanity configuration with draft mode support

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {presentationTool} from 'sanity/presentation'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,18 @@ export default defineConfig({
   projectId: '4vvi2ol1',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    presentationTool({
+      previewUrl: {
+        origin: 'https://piusprince.vercel.app/',
+        previewMode: {
+          enable: '/api/draft-mode/enable',
+        },
+      },
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
