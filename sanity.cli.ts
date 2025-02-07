b4d62a88242380acda3186dbb2ee3dc052ee1abc
Pius Prince <piusprince61@gmail.com>
2025-02-07 20:37:08 +0000

    feat: enhance homepage and project schemas with featured projects and image positioning options

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4vvi2ol1',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost: 'piusprince-sanity-portfolio',
 })
